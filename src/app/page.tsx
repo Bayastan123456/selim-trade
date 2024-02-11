c37dc1dbcaaf613f1f06b4b79c12cef524235c6a
Asan Turdukulov <asanturdukulov86@gmail.com>
2024-02-11 19:25:44 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
+import Advantages from "@/components/Advantages/Advantages";
 import Main from "@/components/Main/Main";
 import Offers from "@/components/Offers/Offers";
-import LastNews from "@/components/LastNews/LastNews";
+// import LastNews from "@/components/LastNews/LastNews";
 import OurTeam from "@/components/OurTeam/OurTeam";
-import Navbar from "@/components/navbar/Navbar";
+import OurWorks from "@/components/OurWorks/OurWorks";
+
+// import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
     <>
       <div className="main_back">
-        <Navbar />
+        {/* <Navbar /> */}
         <Main />
       </div>
       <OurTeam />
-      <LastNews />
+      {/* <LastNews /> */}
       <Offers />
+      <OurWorks />
+      <Advantages/>
     </>
   );
 }
