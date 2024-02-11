ed5de559b0250e9fa73021881985e95b85698a47
Iliya <gene_ral9904@mail.ru>
2024-02-11 21:11:54 +0600

    fix footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Advantages from "@/components/Advantages/Advantages";
 import OurWorks from "@/components/OurWorks/OurWorks";
 import OurTeam from "@/components/OurTeam/OurTeam";
 import Footer from "@/components/Footer/Footer";
+import Form from "@/components/Form/Form";
 export default function Home() {
   return (
     <>
     <OurTeam/>
       <Advantages />
       <OurWorks />
+      <Form/>
       <Footer/>
     </>
   );
