fbe5ec22dc17bf7f1721c7f83dd4535718a398ab
Bayastan123456 <127396034+Bayastan123456@users.noreply.github.com>
2024-02-13 17:37:19 +0300

    Update page.tsx
    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,21 @@ import Advantages from "@/components/Advantages/Advantages";
 import OurWorks from "@/components/OurWorks/OurWorks";
 import OurTeam from "@/components/OurTeam/OurTeam";
 import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 import Form from "@/components/Form/Form";
 export default function Home() {
   return (
     <>
-    <OurTeam/>
-      <Advantages />
-      <OurWorks />
+      <div className="main_back">
+        <Navbar />
+        <Main />
+      </div>
+      <OurTeam />
+      <LastNews />
+      <div className="form_back">
       <Form/>
-      <Footer/>
+        <Footer/>
+      </div>
     </>
   );
 }
