221c7b913157869d5de00b6147c3c7f3ba31b9ec
Bayastan123456 <127396034+Bayastan123456@users.noreply.github.com>
2024-03-16 13:35:35 +0300

    news page

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -26,8 +26,8 @@ export default function OurNewsPage() {
     <>
       <Navbar isMobile={isMobile} />
       <OurNewsPageInside />
-      <Form />
-      <Footer />
+      {/* <Form />
+      <Footer /> */}
     </>
   );
 }
